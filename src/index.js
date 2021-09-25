5c8daa5f26a5e70f51c03e1343c5f98c1eed33fa
Jzapata <jhonny.z1022@gmail.com>
2021-09-24 22:13:09 -0500

    variables

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Variables, Unicamente se usaran mediante let o const
+// Scope o Contexto
+
+const nombre = 'Jhonny';
+const apellido = 'Zapata';
+
+let valorDado = 5;
+// let valorDado = 6;
+
+
+console.log(nombre, apellido, valorDado);
+
+if (true) {
+    let valorDado = 6;
+    console.log(valorDado);
+}
+
+console.log(valorDado)
