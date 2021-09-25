83e6ab64e17f02c899454dbecf9adc5c5aa7359a
Jzapata <jhonny.z1022@gmail.com>
2021-09-24 22:51:27 -0500

    arreglos

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// Arreglos -> coleccion de informacion
+// para crear arreglos literales se deben utilizar las llavez cuadradas
+// esta forma se usa cuando se quiere inicializar de inmediato el arreglo
+// con alguna cantidad de indices por defecto
+// const arreglo = new Array();
+
+// const arreglo = [];
+// arreglo.push(1);
+
+// let arreglo2 = arreglo;
+// arreglo2.push(5);
+
+// console.log(arreglo);
+// console.log(arreglo2);
+
+/**------------------------------------------- */
+
+// const arreglo = [1, 2, 3, 4];
+// // arreglo.push(1);
+
+// // para utilizar el operador spread con arreglos ponemos llaves cuadradas []
+// let arreglo2 = [arreglo, 5];
+// // arreglo2.push(5);
+
+// console.log(arreglo);
+// console.log(arreglo2);
+
+
+/**------------------------------------------- */
+
+const arreglo = [1, 2, 3, 4];
+// arreglo.push(1);
+
+// para utilizar el operador spread con arreglos ponemos llaves cuadradas []
+let arreglo2 = [...arreglo, 5];
+
+const arreglo3 = arreglo2.map( function(numero) {
+    return numero * 2;
+});
+
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3);
