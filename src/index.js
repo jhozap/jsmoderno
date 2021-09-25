796a01a7093ab83e6334b0d76707ed05b4f3e28c
Jzapata <jhonny.z1022@gmail.com>
2021-09-25 08:10:47 -0500

    async await

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const getUrlPromesa = () => {
+    const promesa = new Promise((resolve) => {
+        resolve('https://google.com.co');
+    });
+
+    return promesa;
+};
+
+getUrlPromesa().then(console.log);
+
+
+// async se puede utilizar solo 
+// con el fin de convertir la funcion en una promesa
+const getUrl = async () => {
+    return 'https://google.com.co';
+};
+
+getUrl().then(console.log);
+
+
+const getImagen = async () => {
+    const apiKey = 'dqovj0uMHqgwLjOPXm9upJU7xdU7SxFW';
+    const respuesta = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    const { data } = await respuesta.json();
+    const { url } = data.images.original;
+
+    const img = document.createElement('img');
+    img.src = url;
+
+    document.body.append(img);
+};
+
+
+// const getImagen = async () => {
+//     try {
+//         const apiKey = 'dqovj0uMHqgwLjOPXm9upJU7xdU7SxFW';
+//         const respuesta = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+//         const { data } = await respuesta.json();
+//         const { url } = data.images.original;
+
+//         const img = document.createElement('img');
+//         img.src = url;
+
+//         document.body.append(img);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+
+getImagen();
