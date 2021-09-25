59f31bda4af1abc719d821ee2b0c0ce0163b27ff
Jzapata <jhonny.z1022@gmail.com>
2021-09-25 09:05:05 -0500

    Objetos Literales

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 // objetos literales
-// const vehiculo = { 
-//     marca: 'Toyota',
-//     sistema: 'Automatico',
-//     cilindraje: 1600,
-// };
+const vehiculo = { 
+    marca: 'Toyota',
+    sistema: 'Automatico',
+    cilindraje: 1600,
+};
 
 
-// console.log(vehiculo);
+console.log(vehiculo);
 // console.table(vehiculo);
+
+const vehiculo2 = { 
+    marca: 'Toyota',
+    sistema: 'Automatico',
+    cilindraje: 1600,
+    ubicacion: {
+        lon: 12.3124,
+        lat: 19.4234
+    }
+};
+
+// // por defecto la consola del navegador me imprime los elementos en orden alfabetico
+console.log(vehiculo2);
+
+
 
 // const vehiculo = { 
 //     marca: 'Toyota',
@@ -19,33 +34,26 @@
 //     }
 // };
 
-// // por defecto la consola del navegador me imprime los elementos en orden alfabetico
-// console.log(vehiculo);
-
-
-
-const vehiculo = { 
-    marca: 'Toyota',
-    sistema: 'Automatico',
-    cilindraje: 1600,
-    ubicacion: {
-        lon: 12.3124,
-        lat: 19.4234
-    }
-};
-
-console.log(vehiculo);
+console.log(vehiculo2);
 
 // asignar de esta manera, no copia el valor si no que copia
 // la referencia al espacio en memoria de la otra variable
-// const vehiculo2 = vehiculo;
+// const vehiculo3 = vehiculo2;
+
+// vehiculo3.marca = 'Nisan';
+
+
 
 // const vehiculo2 = { marca: 'Nisan',  };
 
 // operador spread para clonar la estructura
-const vehiculo2 = { ...vehiculo  };
+const vehiculo3 = { ...vehiculo2, modelo: 2016  };
 
-vehiculo2.marca = 'Nisan';
+vehiculo3.marca = 'Nisan';
 
+console.log(vehiculo3);
 console.log(vehiculo2);
-console.log(vehiculo);
+
+
+// console.log(vehiculo2);
+// console.log(vehiculo);
