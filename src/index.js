4cb9fca33a2cf364eafa10b7385f8f2c60d4fd08
Jzapata <jhonny.z1022@gmail.com>
2021-09-28 20:41:35 -0500

    try catch

--- src/index.js
+++ src/index.js
@@ -12,39 +12,39 @@ getUrlPromesa().then(console.log);
 // async se puede utilizar solo 
 // con el fin de convertir la funcion en una promesa
 const getUrl = async () => {
-    return 'https://google.com.co';
+    return 'https://google.com.com';
 };
 
 getUrl().then(console.log);
 
 
-const getImagen = async () => {
-    const apiKey = 'dqovj0uMHqgwLjOPXm9upJU7xdU7SxFW';
-    const respuesta = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
-    const { data } = await respuesta.json();
-    const { url } = data.images.original;
-
-    const img = document.createElement('img');
-    img.src = url;
-
-    document.body.append(img);
-};
-
-
 // const getImagen = async () => {
-//     try {
-//         const apiKey = 'dqovj0uMHqgwLjOPXm9upJU7xdU7SxFW';
-//         const respuesta = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
-//         const { data } = await respuesta.json();
-//         const { url } = data.images.original;
+//     const apiKey = 'dqovj0uMHqgwLjOPXm9upJU7xdU7SxFW';
+//     const respuesta = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+//     const { data } = await respuesta.json();
+//     const { url } = data.images.original;
 
-//         const img = document.createElement('img');
-//         img.src = url;
+//     const img = document.createElement('img');
+//     img.src = url;
 
-//         document.body.append(img);
-//     } catch (error) {
-//         console.log(error);
-//     }
+//     document.body.append(img);
 // };
+
+
+const getImagen = async () => {
+    try {
+        const apiKey = 'dqovj0uMHqgwLjOPXm9upJU7xdU7SxFW';
+        const respuesta = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const { data } = await respuesta.json();
+        const { url } = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append(img);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 getImagen();
